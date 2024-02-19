e0232c242603a078bf3f01d2c73a652466d1eeef
Deepak Patidar <140416404+Patidardeepak1@users.noreply.github.com>
2024-02-19 11:42:28 +0530

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,9 +4,10 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   server:{
+     host: 'https://blood-finder1.onrender.com'
     proxy:{
       '/api':{
-        target:"https://blood-finder1.onrender.com",
+        target:"http://localhost:3000",
         secure:false,
       },
     },
