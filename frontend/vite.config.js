7d18d2686f3b878b59ee04eb87bfca7dfee45778
Deepak Patidar <140416404+Patidardeepak1@users.noreply.github.com>
2024-02-19 19:38:53 +0530

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
     
     proxy:{
       '/api':{
-        target:"https://blood-finder1.onrender.com/",
+        target:"http://localhost:3000",
         secure:false,
       },
     },
