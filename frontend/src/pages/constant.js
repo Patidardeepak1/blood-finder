5990172c9c2ad0b44be46b74943f6fba9af2e3e3
Deepak Patidar <140416404+Patidardeepak1@users.noreply.github.com>
2024-02-19 12:29:19 +0530

    Update constant.js

--- frontend/src/pages/constant.js
+++ frontend/src/pages/constant.js
@@ -1,2 +1,2 @@
-// export const server = "https://blood-finder1.onrender.com";
-export const server = "http://localhost:3000";
+export const server = "https://blood-finder1.onrender.com";
+//export const server = "http://localhost:3000";
